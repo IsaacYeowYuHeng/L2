2b54d1bb52a8c1408909552bee7912e5006a64cf
22021538 <22021538@myrp.edu.sg>
2024-10-26 21:34:12 +0800

    commit

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import React from 'react';
+import {View, Text, Image, ScrollView} from 'react-native';
+import Movie from './components/Movie';
+const AllMovies = () => {
+    return (
+        <ScrollView>
+        <View>
+            <Text></Text>
+            <Text></Text>
+            <Movie title="Doctor Sleep" year={2019} icon_name="skull" poster={require('./img/doctor-sleep.jpg')}/>
+            <Movie title="Midway" year={2020} icon_name="person-rifle" poster={require('./img/midway.jpg')}/>
+        </View>
+        </ScrollView>
+    );
+};
+export default AllMovies;
